41090364fd64198377ca443001083c6dfa0d4b48
martinkoling <martin.koling@kcl.ac.uk>
2017-09-27 00:45:03 +0100

    Mark 0-7 done

--- SingleCharacterEdit.java
+++ SingleCharacterEdit.java
@@ -1,7 +1,63 @@
 public class SingleCharacterEdit {
 
     public static String singleCharacterEditWillTurnAIntoB(String a, String b) {
-        // TODO: your code goes here, replacing the next line with your actual code
+
+
+        String [] singlechara = a.split("");
+        String [] singlecharb = b.split("");
+
+        // replace
+        if(singlechara.length == singlecharb.length) {
+            for (int i = 0; i < singlechara.length; i++) {
+
+                if (!singlechara[i].equals(singlecharb[i])) {
+
+                    return "replace," + i + "," + singlecharb[i];
+
+                }
+
+            }
+        }
+
+        //remove
+        if(singlechara.length-1 == singlecharb.length){
+
+            for (int j = 0; j < singlechara.length; j++) {
+
+                if (!singlechara[j].equals(singlecharb[j])) {
+
+                    return "remove," + j;
+
+                }
+
+
+            }
+        }
+
+        //insert
+        if(singlechara.length+1 == singlecharb.length){
+
+            for (int k = 0; k < singlechara.length; k++){
+
+
+
+                if (!singlechara[k].equals(singlecharb[k])) {
+
+                    return "insert," + k + "," + singlecharb[k];
+
+                }
+
+
+                if(k == singlechara.length-1){
+                    return "insert," + singlechara.length + "," + singlecharb[singlechara.length];
+                }
+
+
+            }
+
+        }
+
+
         return null; // this is only here to make it compile
     }
 
