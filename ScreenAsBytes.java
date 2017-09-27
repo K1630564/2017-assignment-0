f110ce3fb99da05d8d046570608f4e3a7420dd3d
martinkoling <martin.koling@kcl.ac.uk>
2017-09-27 18:23:55 +0100

    Draw a single pixel method done

--- ScreenAsBytes.java
+++ ScreenAsBytes.java
@@ -1,7 +1,25 @@
 public class ScreenAsBytes {
 
     public static void setPixel(byte[] screen, int width, int x, int y) {
-        // TODO complete this code
+
+        int height = screen.length / (width/8);
+
+
+        // what bit in the byte to edit
+        byte findbit = (byte) (1 << (8 - (x+1)));
+
+
+        // which byte to edit
+        if(x < (width / 2)) {
+            screen[((y + y))] = findbit;
+        }
+
+        if(x >= (width/2)){
+            screen[((y + y) + 1)] =findbit;
+
+        }
+
+
     }
 
     public static void drawHorizontalLine(byte[] screen, int width, int startX, int endX, int y) {
