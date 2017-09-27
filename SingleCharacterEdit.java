5136fd5704ea4fb5759a32cdee1e9c843810bb3a
martinkoling <martin.koling@kcl.ac.uk>
2017-09-27 09:46:29 +0100

    Mark 0-7 done all bugs fixed

--- SingleCharacterEdit.java
+++ SingleCharacterEdit.java
@@ -6,12 +6,25 @@ public class SingleCharacterEdit {
         String [] singlechara = a.split("");
         String [] singlecharb = b.split("");
 
+        // single edit check
+        if (singlechara.length == singlecharb.length) {
+            int count = 0;
+            for (int z = 0; z < singlechara.length; z++) {
+                if (!singlechara[z].equals(singlecharb[z])) {
+                    count++;
+                }
+                if (count > 1) {
+                    return null;
+
+                }
+            }
+        }
+
         // replace
-        if(singlechara.length == singlecharb.length) {
+        if (singlechara.length == singlecharb.length) {
             for (int i = 0; i < singlechara.length; i++) {
 
                 if (!singlechara[i].equals(singlecharb[i])) {
-
                     return "replace," + i + "," + singlecharb[i];
 
                 }
@@ -19,46 +32,62 @@ public class SingleCharacterEdit {
             }
         }
 
-        //remove
-        if(singlechara.length-1 == singlecharb.length){
+        // remove
+        if (singlechara.length - 1 == singlecharb.length) {
 
-            for (int j = 0; j < singlechara.length; j++) {
+            for (int j = 0; j < singlecharb.length; j++) {
 
                 if (!singlechara[j].equals(singlecharb[j])) {
 
-                    return "remove," + j;
+                    String tempwordd = a.substring(0, j) + a.substring(j+1);
+
+
+                    if (tempwordd.equals(b)) {
+
+                        return "remove," + j;
+                    }
+
+
+
+                    return null;
+
 
                 }
-
 
             }
         }
 
-        //insert
-        if(singlechara.length+1 == singlecharb.length){
+        // insert
+        if (singlechara.length + 1 == singlecharb.length) {
 
-            for (int k = 0; k < singlechara.length; k++){
-
-
+            for (int k = 0; k < singlechara.length; k++) {
 
                 if (!singlechara[k].equals(singlecharb[k])) {
 
-                    return "insert," + k + "," + singlecharb[k];
+                    char temp = b.charAt(k);
+                    String tempword = a.substring(0, k) + temp + a.substring(k);
+                    if (tempword.equals(b)) {
+
+                        return "insert," + k + "," + singlecharb[k];
+
+                    }
 
                 }
+                //insert after last char
+                if (k == singlechara.length - 1) {
+                    char temp = b.charAt(singlechara.length);
+                    String tempword = a.substring(0, singlechara.length) + temp;
+                    if (tempword.equals(b)) {
 
-
-                if(k == singlechara.length-1){
-                    return "insert," + singlechara.length + "," + singlecharb[singlechara.length];
+                        return "insert," + singlechara.length + "," + singlecharb[singlechara.length];
+                    }
                 }
-
 
             }
 
         }
 
-
-        return null; // this is only here to make it compile
+        return null;
     }
 
 }
