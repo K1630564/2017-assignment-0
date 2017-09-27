12235fec13a6a84b2bbd29d0158bebb42cf10daf
martinkoling <martin.koling@kcl.ac.uk>
2017-09-28 00:50:46 +0100

    fixed minor thing in setPixel

--- ScreenAsBytes.java
+++ ScreenAsBytes.java
@@ -2,20 +2,20 @@ public class ScreenAsBytes {
 
     public static void setPixel(byte[] screen, int width, int x, int y) {
 
-        int height = screen.length / (width/8);
+        int height = screen.length / (width / 8);
 
 
         // what bit in the byte to edit
-        byte findbit = (byte) (1 << (8 - (x+1)));
+        byte findbit = (byte) (1 << (8 - (x + 1)));
 
 
         // which byte to edit
-        if(x < (width / 2)) {
+        if (x < (width / (width/8))) {
             screen[((y + y))] = findbit;
         }
 
-        if(x >= (width/2)){
-            screen[((y + y) + 1)] =findbit;
+        if (x >= (width / (width/8))) {
+            screen[((y + y) + 1)] = findbit;
 
         }
 
@@ -23,8 +23,63 @@ public class ScreenAsBytes {
     }
 
     public static void drawHorizontalLine(byte[] screen, int width, int startX, int endX, int y) {
-        // TODO complete this code
-    }
+
+        // draw line
+
+
+
+
+
+
+        for (int z = startX; z <= endX; z++) {
+            byte draw = (byte) (1 << (8 - (z +1)));
+
+            System.out.println(draw);
+            // byte draw = (byte) ( 4 << (int) (((Math.ceil(((y * width + z) / 8)))) - 1));
+
+
+            screen[(int) (Math.ceil((((y * width) + z) / 8))) - 1] =draw;
+            ;
+
+
+               // byte draw = (byte) (1 << (8 - (startX + z)));
+
+               // if(z == width / (y +1)){
+
+                    // z = z - (width / (y +1));
+                }
+
+
+
+               // System.out.println(draw);
+
+               // System.out.println((((y * width) + z)/8) + " -  ");
+
+
+
+
+
+
+           // if (z >= 8) {
+             //   byte drawsecond = (byte) (1 << (8 - ((z-8) +1 )));
+
+               // System.out.println(drawsecond);
+            //    screen[((y + y) + 1)] = drawsecond;
+           // }
+
+
+        }
+
+
+
+
+
+
+
+
+
+//}
+
     
     public static void drawVerticalLine(byte[] screen, int width, int x, int startY, int endY) {
         // TODO complete this code
